96a8c6ba116540a67dd92ef191e27d2ede0c9c35
Joni Santos <jonisantos1996@hotmail.com>
2025-07-03 17:36:50 +0000

    [build] changing versions and paths

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,9 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.utils import generate_sitemap, APIException
+from src.api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users, Institutions, Connections, Sources, Balances, Categories, Transactions, FixedExpenses, Budgets
+from src.api.models import db, Users, Institutions, Connections, Sources, Balances, Categories, Transactions, FixedExpenses, Budgets
 from datetime import datetime, timezone, timedelta
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
