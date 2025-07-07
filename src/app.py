25c2d7721a360149c0c44ff55cf1cc75fefa416f
Joni Santos <jonisantos1996@hotmail.com>
2025-07-07 16:10:06 +0000

    [fix] paths

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ import cloudinary
 from flask_cors import CORS
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
-static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public')
+static_file_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'public')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
